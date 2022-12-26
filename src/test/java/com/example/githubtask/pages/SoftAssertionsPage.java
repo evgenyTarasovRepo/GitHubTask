5555800c614c5ce37e666c76363b0fd21578a49a
evgenyTarasovRepo <eatarasov@yahoo.com>
2022-12-26 20:52:12 +0300

    удалил лишний код и комментарии

--- src/test/java/com/example/githubtask/pages/SoftAssertionsPage.java
+++ src/test/java/com/example/githubtask/pages/SoftAssertionsPage.java
@@ -9,13 +9,10 @@ public class SoftAssertionsPage {
     private final SelenideElement junitH4 = $x("//h4[contains(text(), 'JUnit5')]");
 
     /**
-     * Из заголовка с примером кода на JUnit5 возвращает строку JUnit5
-     * тем самым подтверждая, что пример с кодом для JUnit5 существует.
+     * Проверка наличия примера для JUnit5.
      * */
     public void getJUnit5Example() {
         junitH4.shouldHave(Condition.text("Using JUnit5"));
 
-//        String[] h4Split = junitH4.text().split(" ");
-//        return h4Split[2];
     }
 }
